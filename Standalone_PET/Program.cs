7c2f5a68c90624e7861cf7ef5b6fdd888c1d495c
Bee Snow <Bee Snow@DESKTOP-6AFGDH5>
2022-08-01 16:17:27 -0500

    Add project files.

---
+++ Standalone_PET/Program.cs
@@ -0,0 +1,27 @@
+﻿using Raylib_cs;
+using static Raylib_cs.Raylib;
+using Standalone_PET.UI;
+
+namespace Standalone_PET
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Button testButton = new Button(65, 25, "Button", 400, 300, Color.GRAY);
+            InitWindow(800, 600, "Standalone Particle Editor Tool");
+
+            while(!WindowShouldClose())
+            {
+                BeginDrawing();
+                ClearBackground(Color.WHITE);
+
+                testButton.Update();
+
+                DrawText("Hello World!", 350, 150, 10, Color.BLACK);
+
+                EndDrawing();
+            }
+        }
+    }
+}
---
+++ Standalone_PET/UI/Button.cs
@@ -0,0 +1,62 @@
+﻿using Raylib_cs;
+using System.Numerics;
+using static Raylib_cs.Raylib;
+
+
+namespace Standalone_PET.UI
+{
+    public class Button
+    {
+        private Rectangle hitbox;
+        public int Width;
+        public int Height;
+        public int X;
+        public int Y;
+        public Color color;
+        public string Text;
+
+        public Button()
+        {
+            Width = 0;
+            Height = 0;
+            Text = "";
+            hitbox = new Rectangle();
+        }
+
+        public Button(int width, int height, string text, int x, int y, Color color)
+        {
+            Width = width;
+            Height = height;
+            Text = text;
+            X = x;
+            Y = y;
+            this.color = color;
+
+            hitbox = new Rectangle(x, y, width, height);
+        }
+
+        public void Update()
+        {
+            DrawRectangle(X, Y, Width, Height, color);
+            if (CheckHover())
+            {
+                DrawRectangle(X, Y, Width, Height, ColorAlpha(Color.BLACK, 0.25f));
+                if (IsMouseButtonDown(MouseButton.MOUSE_BUTTON_LEFT))
+                {
+                    OnClick();
+                }
+            }
+        }
+
+        public bool CheckHover()
+        {
+            int MouseX = GetMouseX();
+            int MouseY = GetMouseY();
+            Vector2 MousePos = new Vector2(MouseX, MouseY);
+
+            return CheckCollisionPointRec(MousePos, hitbox);
+        }
+
+        public virtual void OnClick(){}
+    }
+}
